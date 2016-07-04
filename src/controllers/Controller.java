39ebd0f5af9f9564122616dd51348fe4c142cfc6
Szabó Máté <larde15@gmail.com>
2016-07-04 12:32:56 +0200

    basic controller created

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,5 +1,34 @@
 package controllers;
+  
+  
+import pojos.User;
 
-public class Controller {
+import java.io.IOException;
 
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RequestMethod;  
+import org.springframework.web.bind.annotation.RestController;  
+  
+@RestController  
+public class Controller {  
+   
+ @RequestMapping(value = "/login", method = RequestMethod.POST,headers="Accept=application/json")  
+ public void checkUser(@RequestBody User user)  
+ {  
+  System.out.println(user.toString());
+  //return true;  
+ }
+ 
+ @RequestMapping(value = "/login", method = RequestMethod.GET,headers="Accept=application/json")  
+ public String sendUser() throws JsonGenerationException, JsonMappingException, IOException  
+ {
+	 ObjectMapper mapper = new ObjectMapper();
+	 User testUser = new User("sajt","pass");
+	 String jsonInString = mapper.writeValueAsString(testUser);
+	 return jsonInString;  
+ }  
 }
