d71071d911fc9de4e1184b0c125b52581291c244
Szabó Máté <larde15@gmail.com>
2016-07-05 21:00:14 +0200

    json converter and password encrpyter class done

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,13 +1,11 @@
 package controllers;
   
   
-import pojos.User;
 
-import java.io.IOException;
+import pojos.UserLogin;
+import security.Encrpyt;
 
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RequestMethod;  
@@ -17,18 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {  
    
  @RequestMapping(value = "/login", method = RequestMethod.POST,headers="Accept=application/json")  
- public void checkUser(@RequestBody User user)  
+ public void checkUser(@RequestBody UserLogin user)  
  {  
-  System.out.println(user.toString());
-  //return true;  
+	 System.out.println(user.toString());
+	 //return true;  
  }
  
  @RequestMapping(value = "/login", method = RequestMethod.GET,headers="Accept=application/json")  
- public String sendUser() throws JsonGenerationException, JsonMappingException, IOException  
+ public String sendUser() 
  {
-	 ObjectMapper mapper = new ObjectMapper();
-	 User testUser = new User("sajt","pass");
-	 String jsonInString = mapper.writeValueAsString(testUser);
-	 return jsonInString;  
+	 return "";  
  }  
 }
