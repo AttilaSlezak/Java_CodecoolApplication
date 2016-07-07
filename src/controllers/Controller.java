8f89d5ade332a992075a787dd903f2c2b0b38677
Slezak Attila <attila.slezak@gmail.com>
2016-07-07 10:23:23 +0100

    Can handler login data received as json

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -5,7 +5,12 @@ package controllers;
 import pojos.UserLogin;
 import security.Encrpyt;
 
+import java.io.IOException;
 
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.util.JSONPObject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;  
 import org.springframework.web.bind.annotation.RequestMethod;  
@@ -15,10 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {  
    
  @RequestMapping(value = "/login", method = RequestMethod.POST,headers="Accept=application/json")  
- public void checkUser(@RequestBody UserLogin user)  
+ public String checkUser(@RequestBody String user) throws JsonParseException, JsonMappingException, IOException  
  {  
-	 System.out.println(user.toString());
-	 //return true;  
+	 ObjectMapper mapper = new ObjectMapper();
+	 UserLogin login = mapper.readValue(user, UserLogin.class);
+	 //System.out.println(user.toString());
+	 //System.out.println(login.toString());
+	 //System.out.println(login.getEmail().toString());
+	 return user.toString();
  }
  
  @RequestMapping(value = "/login", method = RequestMethod.GET,headers="Accept=application/json")  
