726c6d323ba8468ac89f00586fe9b263b6e587e3
Daniel Koics <koics.dani@gmail.com>
2016-07-08 16:03:58 +0200

    Make LoginController sign success with a separate JSON object Wire email-sender to forgotten password handler

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import pojos.Error;
+import pojos.Success;
 import pojos.UserLogin;
 import security.Encrypt;
 
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+import javax.mail.MessagingException;
 import javax.servlet.http.HttpSession;
 
 import json.JsonConverter;
 import mappers.UserMapper;
+import password.GmailSender;
+import password.PasswordGenerator;
 
 @RestController
 public class LoginController {
@@ -21,6 +28,7 @@ public class LoginController {
 	UserMapper usermapper;
 	JsonConverter jconverter;
 	Encrypt encrypter;
+	GmailSender emailSender;
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST, headers = "Accept=application/json")
 	public String handleLogin(@RequestBody String jsonUserString,HttpSession session){
@@ -39,8 +47,7 @@ public class LoginController {
 				if (encrypter.compareEncryptedAndRaw(rawPassword, encryptedPassword)) {
 					session.setAttribute("user", dbUser.getEmail());
 					session.setMaxInactiveInterval(45);
-					//Now we need to give him session and return his data.
-					return "true";
+					return jconverter.ConvertObjectToJsonString(new Success("login", dbUser.getEmail())); 
 				}
 				return jconverter.ConvertObjectToJsonString(new Error("login","Password does not match."));
 			}
@@ -56,8 +63,18 @@ public class LoginController {
 		if (jsonUser != null) {
 			UserLogin dbUser = usermapper.getUserLoginByEmail(jsonUser.getEmail());
 			if (dbUser != null) {
-				//Send email to the dbuser.getEmail();
-				return "";
+				String pswd = PasswordGenerator.GeneratePassword();
+				String encrypted = encrypter.encryptPassword(pswd);
+				try
+				{
+					usermapper.updatePassword(dbUser.getEmail(), encrypted);
+					emailSender.sendPassword(dbUser.getEmail(), pswd);
+					return jconverter.ConvertObjectToJsonString(new Success("forgottenPassword", dbUser.getEmail()));
+				}
+				catch(MessagingException | IOException e) {
+					e.printStackTrace();
+					return jconverter.ConvertObjectToJsonString(new Error("forgottenPassword", "unable to send email"));
+				}
 			}
 			return jconverter.ConvertObjectToJsonString(new Error("forgottenPassword","No user with this email."));
 		}
@@ -65,5 +82,12 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value = "/keepalive", method = RequestMethod.POST)
-	public void dummyFunction() { }
+	public String dummyFunction(HttpSession session) {
+		if (session.getAttribute("user") == null)
+			return jconverter.ConvertObjectToJsonString(new Error("keepalive", "You are not logged in."));
+		UserLogin dbUser = usermapper.getUserLoginByEmail((String)session.getAttribute("user"));
+		if (dbUser == null)
+			return jconverter.ConvertObjectToJsonString(new Error("keepalive", "You are not logged in."));
+		return jconverter.ConvertObjectToJsonString(new Success("keepalive", dbUser.getEmail()));
+	}
 }
--- src/mappers/UserMapper.java
+++ src/mappers/UserMapper.java
@@ -10,5 +10,7 @@ public interface UserMapper {
 	//@Select("SELECT * FROM APPLICANTS WHERE EMAIL = #{email}")
 	public UserLogin getUserLoginByEmail(String email);
 	public User getUserByEmail(String email);
-	public void insertUser();
+	public void insertUser();  //TODO make these request complete and try it
+	public void updatePassword(String email, String password);  //TODO try this request and fix up-coming issues
+	// TODO how to sign if a request is unsuccessful (especially if the function has a return type of void)? Maybe an (unchecked) exception? Or a boolean value?
 }
