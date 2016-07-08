d3f5f6419aecd029615553d98d9703666b3842f2
Szabó Máté <larde15@gmail.com>
2016-07-08 10:42:59 +0200

    controller extended

--- src/json/JsonConverter.java
+++ src/json/JsonConverter.java
@@ -3,11 +3,14 @@ package json;
 import java.io.IOException;
 
 import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import pojos.UserLogin;
+
 public class JsonConverter {
-	private String ConvertObjectToJsonString(Object object) {
+	public String ConvertObjectToJsonString(Object object) {
 		ObjectMapper mapper = new ObjectMapper();
 		String jsonInString = null;
 		try {
@@ -20,5 +23,20 @@ public class JsonConverter {
 			e.printStackTrace();
 		}
 		return jsonInString;  
+	}
+	
+	public UserLogin ConvertJsonToUserLoginObject(String json){
+		ObjectMapper mapper = new ObjectMapper();
+		UserLogin login = null;
+		try {
+			login = mapper.readValue(json, UserLogin.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return login;
 	}
 }
---
+++ src/pojos/Error.java
@@ -0,0 +1,21 @@
+package pojos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class Error {
+	Map<String, String> error;
+
+	public Error(String errorType,String errorMsg) {
+		this.error = new HashMap<String, String>();
+		error.put("type",errorType);
+		error.put("errormsg", errorMsg);
+	}
+	public Map<String, String> getError() {
+		return error;
+	}
+	public void setError(Map<String, String> error) {
+		this.error = error;
+	}
+}
